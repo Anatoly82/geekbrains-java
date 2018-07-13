d5a33fc6cd039a33849dd0d9f810cde98a482a8c
Anatoly Yakovlev <yakovlef@gmail.com>
2018-07-13 14:25:37 +0300

    Основное задание по курсу Java2, lesson2

---
+++ HW/exception/CustomException.java
@@ -0,0 +1,11 @@
+package lesson2.HW.exception;
+
+/**
+ * @author Anatoly Yakovlev
+ */
+public class CustomException extends Exception {
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
---
+++ HW/exception/MyArraySizeException.java
@@ -0,0 +1,11 @@
+package lesson2.HW.exception;
+
+/**
+ * @author Anatoly Yakovlev
+ */
+public class MyArraySizeException extends CustomException {
+
+    public MyArraySizeException() {
+        super("The matrix must be of size 4x4");
+    }
+}
